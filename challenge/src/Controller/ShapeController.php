3a22a1ddbbc18646bdbd69e07ab734d62d395f3b
aysar <aysarhajaj@gmail.com>
2022-10-14 23:56:42 +0400

    add two  apis for circle and  triangle

--- challenge/src/Controller/ShapeController.php
+++ challenge/src/Controller/ShapeController.php
@@ -2,10 +2,41 @@
 
 namespace App\Controller;
 
+use App\Model\Circle;
+use App\Model\Triangle;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ShapeController extends AbstractController
 {
+    /**
+     * @Route("/triangle/{a}/{b}/{c}", name="triangle", methods="GET")
+     */
+    public function getTriangleData($a, $b, $c): JsonResponse
+    {
+        $triangle = new Triangle($a, $b, $c);
+        return $this->json([
+            "type" => "triangle",
+            "a" => $a,
+            "b" => $b,
+            "c" => $c,
+            "surface" => $triangle->calculateSurface(),
+            "circumference" => $triangle->calculateCircumference(),
+        ]);
+    }
+
+    /**
+     * @Route("/circle/{radius}", name="circle", methods="GET")
+     */
+    public function getCircleData($radius): JsonResponse
+    {
+        $circle = new Circle($radius);
+        return $this->json([
+            "type" => "circle",
+            "radius" =>  $radius,
+            "surface" => $circle->calculateSurface(),
+            "circumference" => $circle->calculateCircumference(),
+        ]);
+    }
 }
